6f1cf6a7add63c67dc7032cd4b28ceb4d4850a1b
logemann <logemann@iastate.edu>
2017-03-22 15:58:38 -0500

    Finished Homework 3

---
+++ Homework/H03_6.m
@@ -0,0 +1,15 @@
+TOL = 1e-6;
+MaxIter = 700;
+for n=[10, 20, 40, 80, 160, 320]
+    x0 = zeros(n*n, 1);
+    b = ones(n*n, 1);
+    A = CreateA(n);
+    [x, k] = linearConjugateGradient(A, b, x0, TOL, MaxIter);
+    disp(k)
+    ek = TOL;
+    e0 = sqrt((x - x0)'*A*(x - x0));
+    e = (ek/(2*e0))^(1.0/k);
+    c = ((e + 1)/(1 - e))^2;
+    disp(c)
+    disp(condest(A))
+end
