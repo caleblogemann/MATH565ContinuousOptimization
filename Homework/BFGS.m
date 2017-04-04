3e63749143c188c0d7c2106c7333946600048fbf
logemann <logemann@iastate.edu>
2017-04-04 17:11:22 -0500

    Continued homework 4

--- Homework/BFGS.m
+++ Homework/BFGS.m
@@ -1,3 +1,15 @@
-function [] = BFGS()
+function [] = BFGS(x0, H0, TOL, MaxIter)
+    k = 0;
+    mstop = 1;
+
+    while k <= MaxIter && mstop
+        p = -H;
+
+        if
+            mstop = 0;
+        else
+            k = k+1;
+        end
+    end
 
 end
