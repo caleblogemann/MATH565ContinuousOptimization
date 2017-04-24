4509b8c00893a5e13f239e1a51eb8356dc5f6da7
Caleb Logemann <caleb.logemann@gmail.com>
2017-04-24 08:37:58 -0500

    Continued Homework 5

---
+++ Homework/H05_4.m
@@ -0,0 +1,15 @@
+n = 2;
+t = [0.0, 0.3, 0.6, 0.9]';
+s = [2.7, 1.48, 0.819, 0.458]';
+x0 = [1, 1, 1, 2]';
+rj = @(j, x) sum(arrayfun(@(k) x(k)*exp(-x(n+k)*t(j)), 1:n)) - s(j);
+r = @(x) arrayfun(@(j) rj(j,x),1:2*n)';
+Jj1 = @(j, x) arrayfun(@(i) exp(-x(n+i)*t(j)),1:n);
+Jj2 = @(j, x) arrayfun(@(i) -t(j)*x(i-n)*exp(-x(i)*t(j)),n+1:2*n);
+Jj = @(j, x) [Jj1(j, x)'; Jj2(j, x)'];
+J = @(x) cell2mat(arrayfun(@(j) Jj(j, x), 1:2*n, 'UniformOutput', false));
+
+TOL = 1e-5;
+MaxIter = 1000;
+
+x = gaussNewton(x0, J, r, TOL, MaxIter);
---
+++ Homework/gaussNewton.m
@@ -0,0 +1,26 @@
+function [x, k] = gaussNewton(x0, J, r, TOL, MaxIter)
+    two_n = length(x0);
+    n = two_n/2;
+
+    x = zeros(two_n, MaxIter+1);
+    x(:,1) = x0;
+
+    k = 1;
+    mstop = 1;
+
+    rx = r(x(:,k));
+    Jx = J(x(:,k))
+    while k <= MaxIter && mstop
+        p = -(Jx'*Jx)\(Jx'*rx);
+        x(:,k+1) = x(:,k) + p;
+
+        k = k+1;
+        rx = r(x(:,k));
+        if norm(rx) < TOL && norm(p)
+            mstop = 0;
+        else
+            Jx = J(x(:,k));
+        end
+    end
+    x = x(:,1:k);
+end
